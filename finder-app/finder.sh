f3f4df5a36648756146ccf0da305b2647975153b
SoumyaCode74 <soumyadeep.work01@gmail.com>
2023-12-04 09:49:25 +0530

    Fix Finder app, Add Writer app

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 if (( $# < 2 )); then
-  echo "Not enough arguments provided. Exiting..."
+  #echo "Not enough arguments provided. Exiting..."
   exit 1
 fi
-declare -r variable1=$1
-declare -r variable2=$2
+declare -r filesdir=$1
+declare -r searchstr=$2
 declare X=0
 declare Y=0
-if [ -d $variable1 ];
+if [ -d $filesdir ];
 then
-  X=$(grep -aRci $variable2 $variable1 | wc -l)
-  for i in $(grep -aRci $variable2 $variable1 | grep -oP [0-9][0-9]); do
-    ((Y+=i))
+  X=$(grep -rc $searchstr $filesdir | wc -l)
+  #Y=$(grep -r "$searchstr" $filesdir | wc -l)
+  for i in $(grep -rc $searchstr $filesdir | grep -oP :[0-9]* | grep -oP [0-9]*); do
+	((Y+=i))
   done
   echo "The number of files are $X and the number of matching lines are $Y"
   exit 0
 else
-  echo "$variable1 is not a directory! Exiting..."
+  #echo "$variable1 is not a directory! Exiting..."
   exit 1
 fi
